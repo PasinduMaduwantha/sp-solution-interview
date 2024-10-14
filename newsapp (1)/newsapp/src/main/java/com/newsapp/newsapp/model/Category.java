8647e10cb00b808405f68ce55d074bb08e76e61f
Pasindu Madhuwantha <73535785+PasinduMaduwantha@users.noreply.github.com>
2024-10-14 14:21:11 +0530

    done

--- newsapp (1)/newsapp/src/main/java/com/newsapp/newsapp/model/Category.java
+++ newsapp (1)/newsapp/src/main/java/com/newsapp/newsapp/model/Category.java
@@ -1,9 +1,34 @@
+//package com.newsapp.newsapp.model;
+//
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import jakarta.persistence.*;
+//import java.util.Set;
+//import com.fasterxml.jackson.annotation.JsonIgnore;
+//
+//@Entity
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//public class Category {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String name;
+//
+//    @ManyToMany(mappedBy = "categories")
+//    private Set<News> news;
+//}
+
+
 package com.newsapp.newsapp.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import jakarta.persistence.*;
 import java.util.Set;
 
 @Entity
@@ -17,5 +42,6 @@ public class Category {
     private String name;
 
     @ManyToMany(mappedBy = "categories")
+    @JsonIgnore
     private Set<News> news;
 }
--- newsapp (1)/newsapp/src/main/java/com/newsapp/newsapp/model/News.java
+++ newsapp (1)/newsapp/src/main/java/com/newsapp/newsapp/model/News.java
@@ -1,10 +1,41 @@
+//package com.newsapp.newsapp.model;
+//
+//import jakarta.persistence.Id;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import jakarta.persistence.*;
+//import java.sql.Timestamp;
+//import java.util.Set;
+//
+//@Entity
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//public class News {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String title;
+//    private String content;
+//    private Timestamp createdAt;
+//
+//    @ManyToMany
+//    @JoinTable(
+//            name = "news_categories",
+//            joinColumns = @JoinColumn(name = "news_id"),
+//            inverseJoinColumns = @JoinColumn(name = "category_id")
+//    )
+//    @JsonManagedReference
+//    private Set<Category> categories;
+//}
+
 package com.newsapp.newsapp.model;
 
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import jakarta.persistence.*;
 import java.sql.Timestamp;
 import java.util.Set;
 
